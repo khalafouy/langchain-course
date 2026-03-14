ed40df28a00ad5347ea48aa10d45f5fbb1e93ac0
mohamedkhalaf <mohamedkhalaf0912@gmail.com>
2026-03-14 17:18:56 +0200

    Final Setup

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List 
+from pydantic import BaseModel,Field
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -17,13 +19,23 @@ load_dotenv()
 # tools = [search]
 
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the response from the agent"""
+    sources: List[Source] = Field(description="The sources used by the agent",default_factory=list)
+    answer: str = Field(description="The answer to the question")
+
+
 tools = []
 tools.append(TavilySearch())
 
+llm = ChatOllama(model="qwen3:1.7b", temperature=0, format=AgentResponse.model_json_schema())
+llm = ChatOpenAI(model="gpt-5.2", temperature=0)
 
-# llm = ChatOpenAI(model="gpt-5", temperature=0)
-llm = ChatOllama(model="qwen3:1.7b", temperature=0)
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
@@ -32,13 +44,11 @@ def main():
         {
             "messages": [
                 HumanMessage(
-                    content="عايز ايات من القران الكريم بتتكلم عن الميراث وعايز تفسيرها "
+                    content="List me 10 opening job vacancies in Egypt Related to Backend Development"
                 )
             ]
         }
     )
-    print(response)
-
 
 if __name__ == "__main__":
     main()
